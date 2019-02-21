45b80ff1e2fa869de4cf4a2ff864aee7a8243aa8
bradenmunn <bradenmunn@yahoo.com>
2019-02-21 12:15:44 -0600

    Updated todo tracker

---
+++ Todo.java
@@ -0,0 +1,21 @@
+public class Todo {/*
+	Lab Section_________: 13
+	Last Modified_______: 2019-02-21
+	OU ID_______________: 113483760
+	OU EMAIL____________: bradenmunn@ou.edu
+
+	Tasks:
+	1. Complete the Shape class compareTo()
+	2. Complete the Circle.java class methods (very short)
+
+	Estimated Time (minutes):
+	1. 10
+	2. 8
+
+	Actual Time (minutes):
+	1. 8
+	2. 6
+
+	*/
+
+}
