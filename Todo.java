582af73536e5e1ecf0d6cb15dc983d04626fd8a7
bradenmunn <bradenmunn@yahoo.com>
2019-02-21 16:11:15 -0600

    Updated todo tracker

--- Todo.java
+++ Todo.java
@@ -8,16 +8,28 @@ public class Todo {/*
 	1. Complete the Shape class compareTo()
 	2. Complete the Circle.java class methods (very short)
 	3. Complete Polygon.java class (one method)
+	4. Complete EquilateralTriangle.java
+	5. Complete Trapezoid.java
+	6. Add and implement Rectangle.java
+	7. Add and implement Square.java
 
 	Estimated Time (minutes):
 	1. 10
 	2. 8
 	3. 5
+	4. 5
+	5. 5
+	6. 8
+	7. 5
 
 	Actual Time (minutes):
 	1. 8
 	2. 6
 	3. 5
+	4. 5
+	5. 5
+	6. 5
+	7. 5
 
 	*/
 
